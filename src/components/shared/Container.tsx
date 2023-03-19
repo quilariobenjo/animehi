6cd37e09e045344cc42d30c013b1665876d609a4
unknown <benjoquilario@gmail.com>
2023-03-20 01:28:13 +0800

    feat: make app responsive

--- src/components/shared/Container.tsx
+++ src/components/shared/Container.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React from 'react';
 
 type ContainerProps = {
   children: React.ReactNode;
 };
 
 const Container: React.FC<ContainerProps> = ({ children }) => {
-  return <div className="m-0	w-full max-w-screen-2xl px-4">{children}</div>;
+  return (
+    <div className="m-0 w-full max-w-screen-2xl px-2 md:px-4">{children}</div>
+  );
 };
 
 export default Container;
